9ad45c1a5f0b3bcbd910726734d9ba3b6f1305ba
Zac Gross <zacgross@gmail.com>
2010-07-06 19:20:18 -0400

    -Added GetOccurrences method

---
+++ TemporalToolkit.Tests/ExpressionBaseTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TemporalToolkit.TemporalExpressions;
+
+namespace TemporalToolkit.Tests
+{
+    [TestFixture]
+    public class ExpressionBaseTests
+    {
+        [Test]
+        public void OccurrencesHour()
+        {
+            TemporalExpression te = new TEHour(3);
+            List<DateTime> dates = te.Occurrences(new DateTime(2010, 6, 1), new DateTime(2010,6,4),IntervalPrecision.Hours);
+            Assert.AreEqual(3,dates.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 3, 0, 0), dates[0]);
+            Assert.AreEqual(new DateTime(2010, 6, 2, 3, 0, 0), dates[1]);
+            Assert.AreEqual(new DateTime(2010, 6, 3, 3, 0, 0), dates[2]);
+
+            List<DateTime> dates2 = te.Occurrences(new DateTime(2010, 7, 1), 3,IntervalPrecision.Hours);
+            Assert.AreEqual(3, dates2.Count);
+            Assert.AreEqual(new DateTime(2010, 7, 1, 3, 0, 0), dates2[0]);
+            Assert.AreEqual(new DateTime(2010, 7, 2, 3, 0, 0), dates2[1]);
+            Assert.AreEqual(new DateTime(2010, 7, 3, 3, 0, 0), dates2[2]);
+
+        }
+
+        [Test]
+        public void OccurrenceMinute()
+        {
+            TemporalExpression te = new TEMinute(45);
+            List<DateTime> dates = te.Occurrences(new DateTime(2010, 6, 1), new DateTime(2010, 6, 2), IntervalPrecision.Minutes);
+            Assert.AreEqual(24, dates.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 45, 0), dates[0]);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 23, 45, 0), dates[23]);
+
+            List<DateTime> dates2 = te.Occurrences(new DateTime(2010, 6, 1), 12, IntervalPrecision.Minutes);
+            Assert.AreEqual(12, dates2.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 45, 0), dates2[0]);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 11, 45, 0), dates2[11]);
+        }
+
+        [Test]
+        public void OccurrenceSecond()
+        {
+            TemporalExpression te = new TESecond(45);
+            List<DateTime> dates = te.Occurrences(new DateTime(2010, 6, 1), new DateTime(2010, 6, 2), IntervalPrecision.Seconds);
+            Assert.AreEqual(1440, dates.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 0, 45), dates[0]);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 23, 59, 45), dates[1439]);
+
+            List<DateTime> dates2 = te.Occurrences(new DateTime(2010, 6, 1), 720, IntervalPrecision.Seconds);
+            Assert.AreEqual(720, dates2.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 0, 45), dates2[0]);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 11, 59, 45), dates2[719]);
+        }
+
+        [Test]
+        public void OccurrenceDay()
+        {
+            TemporalExpression te = new TEDayInMonth(3);
+            List<DateTime> dates = te.Occurrences(new DateTime(2010, 6, 1), new DateTime(2011, 6, 5), IntervalPrecision.Days);
+            Assert.AreEqual(13, dates.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 3, 0, 0, 0), dates[0]);
+            Assert.AreEqual(new DateTime(2011, 6, 3, 0, 0, 0), dates[12]);
+
+            List<DateTime> dates2 = te.Occurrences(new DateTime(2010, 6, 1), 12, IntervalPrecision.Days);
+            Assert.AreEqual(12, dates2.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 3, 0, 0, 0), dates2[0]);
+            Assert.AreEqual(new DateTime(2011, 5, 3, 0, 0, 0), dates2[11]);
+        }
+
+        [Test]
+        public void OccurrenceWeek()
+        {
+            TemporalExpression te = new TEInterval(new DateTime(2010, 6, 1), 1, IntervalPrecision.Weeks);
+            
+            List<DateTime> dates = te.Occurrences(new DateTime(2010, 6, 1), new DateTime(2011, 6, 5), IntervalPrecision.Weeks);
+            Assert.AreEqual(53, dates.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 0, 0), dates[0]);
+            Assert.AreEqual(new DateTime(2011, 5, 31, 0, 0, 0), dates[52]);
+
+            List<DateTime> dates2 = te.Occurrences(new DateTime(2010, 6, 1), 12, IntervalPrecision.Weeks);
+            Assert.AreEqual(12, dates2.Count);
+            Assert.AreEqual(new DateTime(2010, 6, 1, 0, 0, 0), dates2[0]);
+            Assert.AreEqual(new DateTime(2010, 8, 17, 0, 0, 0), dates2[11]);
+        }
+
+    }
+}
--- TemporalToolkit/TemporalExpressions/TemporalExpression.cs
+++ TemporalToolkit/TemporalExpressions/TemporalExpression.cs
@@ -68,5 +68,91 @@ namespace TemporalToolkit.TemporalExpressions
             return exprA.Or(exprB);
         }
 
+
+        
+                
+       
+        /// <summary>
+        /// Get specified number of occurrences of TE after specified date.
+        /// </summary>
+        /// <param name="start">Start date</param>
+        /// <param name="maxOccurrences">Number of occurrences to return</param>
+        /// <param name="precision">For performance use the longest value possible.</param>
+        /// <returns></returns>
+        public List<DateTime> Occurrences(DateTime start, int maxOccurrences, IntervalPrecision precision)
+        {
+            return Occurrences(start, null, maxOccurrences, precision);
+        }
+       
+        /// <summary>
+        /// Get occurrences of TE between ranage of dates.
+        /// </summary>
+        /// <param name="rangeStart">Start of range</param>
+        /// <param name="rangeEnd">End of range</param>
+        /// <param name="precision">For performance use the longest value possible.</param>
+        /// <returns></returns>
+        public List<DateTime> Occurrences(DateTime rangeStart, DateTime rangeEnd, IntervalPrecision precision)
+        {
+            return this.Occurrences(rangeStart, rangeEnd, 0, precision);
+        }
+
+        /// <summary>
+        /// Returns a list of occurrences between 2 dates or a max number of occurrences from start date
+        /// based on the maxOccurrences param.
+        /// </summary>
+        /// <param name="rangeStart">Start of date range</param>
+        /// <param name="rangeEnd">End of date range. Must be specified if maxOccurrences is 0</param>
+        /// <param name="maxOccurrences">Max number of occurrences to return. Default 0 means infinte.
+        /// rangeEnd must be set if maxOccurrences is 0.
+        /// </param>
+        /// <param name="precision">For performance use the highest value possible. If using time temporal
+        /// expressions (hour,min,sec) increment should be set accordingly. Valid values are
+        /// Days, Hours, Mins, Secs, Weeks.
+        /// </param>
+        /// <returns></returns>
+        public List<DateTime> Occurrences(DateTime rangeStart, DateTime? rangeEnd, int maxOccurrences, IntervalPrecision precision)
+        {
+            if (!rangeEnd.HasValue && maxOccurrences < 1) 
+                throw new ArgumentException("Must specify end of range or max occurrences to return.");
+            
+            List<DateTime> occurrences = new List<DateTime>();
+            TimeSpan ts;
+            switch (precision)
+            {
+                case IntervalPrecision.Days:
+                    ts = new TimeSpan(1, 0, 0, 0);
+                    break;
+                case IntervalPrecision.Hours:
+                    ts = new TimeSpan(1, 0, 0);
+                    break;
+                case IntervalPrecision.Minutes:
+                    ts = new TimeSpan(0, 1, 0);
+                    break;
+                case IntervalPrecision.Seconds:
+                    ts = new TimeSpan(0, 0, 1);
+                    break;
+                case IntervalPrecision.Weeks:
+                    ts = new TimeSpan(7, 0, 0, 0);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            DateTime counter = new DateTime(rangeStart.Ticks);
+            if (!rangeEnd.HasValue) 
+                rangeEnd = DateTime.MaxValue;
+
+            while (counter <= rangeEnd)
+            {
+                if (this.Includes(counter)) 
+                    occurrences.Add(counter);
+                counter = counter.Add(ts);
+                if (maxOccurrences > 0 && occurrences.Count == maxOccurrences)
+                    break;
+            }
+
+            return occurrences;
+        }
+
     }
 }
